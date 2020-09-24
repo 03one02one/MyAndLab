6d31a77cc86f852b8076c9989a034d1c3de6e911
03one02one <ehrkd1021@naver.com>
2020-09-24 21:04:47 +0900

    p103 버튼 실행 예제

---
+++ app/src/main/java/kr/hs/emirim/one02one/myandlab/FourbuttonActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.one02one.myandlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FourbuttonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fourbutton)
+    }
+}
